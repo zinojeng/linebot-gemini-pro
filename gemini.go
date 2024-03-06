eea8310db90c271f7e24cc29d04d3ee8df036fc8
zinojeng <zino@livemail.tw>
2024-03-06 10:29:44 +0800

    Update gemini.go

--- gemini.go
+++ gemini.go
@@ -26,7 +26,7 @@ func GeminiImage(imgData []byte) (string, error) {
 	model.Temperature = &value
 	prompt := []genai.Part{
 		genai.ImageData("png", imgData),
-		genai.Text("Describe this image with scientific detail, reply in zh-TW:"),
+		genai.Text("As an expert specializing in diabetic diatician and assessing the suitability of fruits and foods for individuals with diabetes, your task involves analyzing input images featuring various food items. Your first objective is to identify the type of fruit or food present in the image. Subsequently, you must determine the glycemic index and the estimation of calories of the identified item. Based on this glycemic index and calories, provide recommendations on whether individuals with diabetes can include the detected food in their diet. If the food is deemed suitable, specify the recommended quantity for consumption. response all in zh-tw"),
 	}
 	log.Println("Begin processing image...")
 	resp, err := model.GenerateContent(ctx, prompt...)
